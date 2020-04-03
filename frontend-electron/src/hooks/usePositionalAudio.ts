f031aa51335e53cdc6a9c5eeff8175c0ba1a540a
JacobLuke <jacob.luke.abrahams@gmail.com>
2020-04-03 10:59:38 -0400

    Make the audio panner actually have positional output

--- frontend-electron/src/hooks/usePositionalAudio.ts
+++ frontend-electron/src/hooks/usePositionalAudio.ts
@@ -1,32 +1,33 @@
 import { useEffect, useCallback, useState } from "react";
 
 export default function usePositionalAudio(listenerPos: { x: number, y: number } | null) {
-    const [ctx] = useState<AudioContext>(new AudioContext);
-    useEffect(() => {
-        if (!listenerPos) {
-            return;
-        }
-        const listener = ctx.listener;
-        listener.positionX.value = listenerPos.x;
-        listener.positionY.value = listenerPos.y;
-    }, [ctx, listenerPos?.x, listenerPos?.y])
     const playPositionalAudio = useCallback((sourcePos: { x: number, y: number }, data: Int16Array) => {
-        const buffer = ctx.createBuffer(1, data.length, 44100);
-        buffer.copyToChannel(new Float32Array(data).map(i => i / 0x7fff), 0);
-        const bufferSource = ctx.createBufferSource();
+        const context = new AudioContext();
+        if (listenerPos) {
+            context.listener.positionX.value = listenerPos.x;
+            context.listener.positionZ.value = listenerPos.y;
+        }
+
+        const buffer = context.createBuffer(2, data.length, 44100);
+        const floatData = new Float32Array(data).map(i => i / 0x7fff);
+        buffer.copyToChannel(floatData, 0);
+        buffer.copyToChannel(floatData, 1);
+        const bufferSource = context.createBufferSource();
         bufferSource.buffer = buffer;
-        const panner = ctx.createPanner();
+        const panner = context.createPanner();
         panner.distanceModel = "inverse";
         panner.refDistance = 5;
         panner.maxDistance = 1000;
         panner.coneInnerAngle = 360;
         panner.coneOuterAngle = 0;
         panner.coneOuterGain = 0;
-        panner.rolloffFactor = 0.1;
+        panner.rolloffFactor = 1;
+        panner.positionX.value = sourcePos.x;
+        panner.positionZ.value = sourcePos.y;
         bufferSource.connect(panner);
-        panner.connect(ctx.destination);
+        panner.connect(context.destination);
         bufferSource.start()
-    }, [ctx]);
+    }, [listenerPos?.x, listenerPos?.y]);
 
     return { playPositionalAudio };
 }
