50e3725e06790a5085abc565b63046c2adb0f074
JacobLuke <jacob.luke.abrahams@gmail.com>
2020-04-06 18:56:33 -0400

    Add a bunch of IceServers to see if that helps

--- frontend-electron/src/socket.ts
+++ frontend-electron/src/socket.ts
@@ -131,7 +131,29 @@ export default class Socket {
         if (this._peerConnections[otherID]) {
             return;
         }
-        const connection = new RTCPeerConnection();
+        const connection = new RTCPeerConnection({
+            iceServers: [
+                { urls: 'stun:stun01.sipphone.com' },
+                { urls: 'stun:stun.ekiga.net' },
+                { urls: 'stun:stun.fwdnet.net' },
+                { urls: 'stun:stun.ideasip.com' },
+                { urls: 'stun:stun.iptel.org' },
+                { urls: 'stun:stun.rixtelecom.se' },
+                { urls: 'stun:stun.schlund.de' },
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:stun3.l.google.com:19302' },
+                { urls: 'stun:stun4.l.google.com:19302' },
+                { urls: 'stun:stunserver.org' },
+                { urls: 'stun:stun.softjoys.com' },
+                { urls: 'stun:stun.voiparound.com' },
+                { urls: 'stun:stun.voipbuster.com' },
+                { urls: 'stun:stun.voipstunt.com' },
+                { urls: 'stun:stun.voxgratia.org' },
+                { urls: 'stun:stun.xten.com' },
+            ]
+        });
         connection.onnegotiationneeded = async () => {
             const offer = await connection.createOffer({ voiceActivityDetection: true });
             await connection.setLocalDescription(offer);
